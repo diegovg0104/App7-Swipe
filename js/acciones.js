130644b5748b0fbf8fbab350ac1babd0c35caf24
madairuiz <madai.ruiz.1g@gmail.com>
2018-05-22 14:32:45 -0500

    4subida

--- js/acciones.js
+++ js/acciones.js
@@ -5,12 +5,12 @@ $(document).ready(function(e) {
 		$('#izquierda').on("swipeleft", function(){
 			navigator.notification.alert("deslizo hacia la izquierda", function(){"aplicacion 7","Ok"});
 			});//cerrar deslizar izquierda 
-		$('#izquierda').on("swiperight", function(){
+		$('#derecha').on("swiperight", function(){
 			navigator.notification.confirm("¿Que quieres hacer?", function(op){
 				switch(op)
 				{
 					case 1:
-					 navigator.notificacion.beep(1);
+					 navigator.notification.beep(1);
 					break;
 					case 2:
 					 navigator.notification.vibrate(3000);
